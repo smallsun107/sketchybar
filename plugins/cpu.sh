9ec3a694703782a4d78e2ffe1db540d1ba497f1c
smallsun <smallsun107@gmail.com>
2026-03-01 01:45:37 +0800

    fix: fix cpu script

--- plugins/cpu.sh
+++ plugins/cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # CPU_PERCENT=$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5 }' | cut -d "." -f1)
-CPU_PERCENT=$(iostat -c 2 | awk 'NR==4 {print int(100 - $9)}')
+CPU_PERCENT=$(iostat -c 2 | awk 'END {print int(100 - $(NF-3))}')
 
 sketchybar --set $NAME label="${CPU_PERCENT}%"
